45c7682be6a0b2d80ebfac737582dc30929773c8
AlinaLaniuk <alinavoronina16@gmail.com>
2023-03-14 16:19:15 +0200

    feat: set up webpack settings

---
+++ react-components/vite.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
