708b11ce82530a61fda332bc2be7a39ff886e42f
AlinaLaniuk <alinavoronina16@gmail.com>
2023-03-17 22:25:37 +0200

    feat: add saving info in local storage logic

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -5,39 +5,60 @@ import cardsData from './data';
 import debounce from './utils';
 import CardProps from './types';
 
-interface IData {
+interface IState {
   data: CardProps[];
+  searchValue: string;
 }
 
-class App extends Component<unknown, IData> {
+const setMatchedInputValueCardsData = (inputValue: string) => {
+  return cardsData.filter((cardData) => {
+    return (
+      cardData.header.toLowerCase().includes(inputValue.toLowerCase()) ||
+      cardData.description.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  });
+};
+
+class App extends Component<unknown, IState> {
   debouncedUpdateCards: (...args: React.ChangeEvent<Element>[]) => void;
 
-  constructor(props: number) {
+  constructor(props: unknown) {
     super(props);
-    this.state = { data: [] };
+    this.state = { data: [], searchValue: '' };
     this.updateCards = this.updateCards.bind(this);
-    this.debouncedUpdateCards = debounce(this.updateCards, 2000);
+    this.debouncedUpdateCards = debounce(this.updateCards, 1000);
   }
 
   componentDidMount() {
-    this.setState({ data: cardsData });
+    let searchValueFromLocalStorage = localStorage.getItem('lastSearchValue');
+    if (!searchValueFromLocalStorage) {
+      searchValueFromLocalStorage = '';
+    }
+    const currentCardsData = setMatchedInputValueCardsData(searchValueFromLocalStorage);
+    this.setState({ data: currentCardsData, searchValue: searchValueFromLocalStorage });
+  }
+
+  componentDidUpdate(prevProps: unknown, prevState: IState) {
+    const { searchValue } = this.state;
+    if (searchValue !== prevState.searchValue) {
+      localStorage.setItem('lastSearchValue', searchValue);
+    }
   }
 
   updateCards(event: React.ChangeEvent) {
     const eventTarget = event.target as HTMLInputElement;
-    const valueInput = eventTarget.value;
-    const cardDataMatchedInputValue = cardsData.filter((cardData) => {
-      return cardData.header.includes(valueInput) || cardData.description.includes(valueInput);
-    });
-    this.setState({ data: cardDataMatchedInputValue });
+    const inputValue = eventTarget.value;
+    const cardDataMatchedInputValue = setMatchedInputValueCardsData(inputValue);
+    this.setState({ data: cardDataMatchedInputValue, searchValue: inputValue });
   }
 
   render() {
     const { data } = this.state;
+    const { searchValue } = this.state;
     return (
       <div className="App">
         <div className="search-bar-container">
-          <SearchBar callback={this.debouncedUpdateCards} />
+          <SearchBar callback={this.debouncedUpdateCards} inputValue={searchValue} />
         </div>
         <div className="cards-container">
           {data.map((cardData) => {
--- react-components/src/components/searchBar/searchBar.tsx
+++ react-components/src/components/searchBar/searchBar.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import CardProps from '../../types';
 
 interface ICallback {
   callback: (event: React.ChangeEvent) => void;
+  inputValue: string;
 }
 
-function SearchBar({ callback }: ICallback) {
+function SearchBar({ callback, inputValue }: ICallback) {
   return (
     <div className="input-wrapper">
       <img src="./search-bar.png" alt="search-bar-icon" />
-      <input onChange={callback} placeholder="Type to search..." type="text" />
+      <input
+        onChange={callback}
+        placeholder="Type to search..."
+        type="text"
+        defaultValue={inputValue}
+      />
     </div>
   );
 }
