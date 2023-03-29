99ab52b4bb407fb7f42f88225a4ef55495be6924
AlinaLaniuk <alinavoronina16@gmail.com>
2023-03-29 12:34:31 +0300

    refactor: add hooks for main page

---
+++ react-components/src/pages/main.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import SearchBar from '../components/searchBar/searchBar';
+import Card from '../components/card/card';
+import cardsData from '../data';
+import debounce from '../utils';
+import { IState, HeaderBoundProps, CardProps } from '../types';
+
+const setMatchedInputValueCardsData = (inputValue: string) => {
+  return cardsData.filter((cardData) => {
+    return (
+      cardData.header.toLowerCase().includes(inputValue.toLowerCase()) ||
+      cardData.description.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  });
+};
+
+function MainPage(props: HeaderBoundProps) {
+  const [searchValue, updateSearchValue] = useState(localStorage.getItem('lastSearchValue') || '');
+  const initialData = setMatchedInputValueCardsData(searchValue);
+  const [data, updateData] = useState(initialData);
+  useEffect(() => {
+    const { setPageName } = props;
+    setPageName('Main');
+  });
+
+  function updateCards(event: React.ChangeEvent) {
+    const eventTarget = event.target as HTMLInputElement;
+    const inputValue = eventTarget.value;
+    updateSearchValue(inputValue);
+    updateData(setMatchedInputValueCardsData(inputValue));
+  }
+
+  const debouncedUpdateCards = debounce(updateCards, 1000);
+
+  return (
+    <>
+      <div className="search-bar-container">
+        <SearchBar callback={debouncedUpdateCards} inputValue={searchValue} />
+      </div>
+      <div data-testid="cards-container" className="cards-container">
+        {data.map((cardData: CardProps) => {
+          return <Card key={cardData.id} {...cardData} />;
+        })}
+      </div>
+    </>
+  );
+}
+export default MainPage;
