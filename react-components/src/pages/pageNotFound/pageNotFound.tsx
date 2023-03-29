67bb39f3c77cd3e2aa89653899e981f65be30905
AlinaLaniuk <alinavoronina16@gmail.com>
2023-03-29 13:41:59 +0300

    refactor: add hooks in page not found and about us pages

--- react-components/src/pages/pageNotFound/pageNotFound.tsx
+++ react-components/src/pages/pageNotFound/pageNotFound.tsx
@@ -1,26 +1,24 @@
 import { Link } from 'react-router-dom';
-import React, { Component } from 'react';
-import { HeaderBoundProps } from '../types';
+import React, { useEffect } from 'react';
+import { HeaderBoundProps } from '../../types';
 
-class PageNotFound extends Component<HeaderBoundProps> {
-  componentDidMount() {
-    const { setPageName } = this.props;
-    setPageName('NotFound');
-  }
+function PageNotFound(props: HeaderBoundProps) {
+  useEffect(() => {
+    const { setPage } = props;
+    setPage('Page not found');
+  });
 
-  render() {
-    return (
-      <div className="page-not-found-container">
-        <h1>Page Not Found</h1>
-        <p>
-          This page doesn`t exist. Go{' '}
-          <Link className="go-home-link" to="/">
-            home
-          </Link>
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="page-not-found-container">
+      <h1>Page Not Found</h1>
+      <p>
+        This page doesn`t exist. Go{' '}
+        <Link className="go-home-link" to="/">
+          home
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default PageNotFound;
