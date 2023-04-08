32b443302b0a52d20a3b18c653225a6e8eb144e3
AlinaLaniuk <alinavoronina16@gmail.com>
2023-04-08 17:20:40 +0300

    feat: add catch errors logic

--- react-components/src/pages/main/services.tsx
+++ react-components/src/pages/main/services.tsx
@@ -1,13 +1,24 @@
 const baseURL = 'https://rickandmortyapi.com/api';
 
 export function getCharacters(searchValue: string) {
-  return fetch(`${baseURL}/character/?name=${searchValue}`).then((response) => {
-    return response.json();
-  });
+  return fetch(`${baseURL}/character/?name=${searchValue}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return undefined;
+    })
+    .catch((error) => {
+      console.error('The Promise is rejected!', error);
+    });
 }
 
 export function getCharacter(id: number) {
-  return fetch(`${baseURL}/character/${id}`).then((response) => {
-    return response.json();
-  });
+  return fetch(`${baseURL}/character/${id}`)
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.error('The Promise is rejected!', error);
+    });
 }
