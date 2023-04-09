077b3197f8fb5dfac8073064768076b4f77526d1
AlinaLaniuk <alinavoronina16@gmail.com>
2023-04-09 12:16:21 +0300

    feat: add loading

--- react-components/src/pages/main/services.tsx
+++ react-components/src/pages/main/services.tsx
@@ -1,6 +1,10 @@
 const baseURL = 'https://rickandmortyapi.com/api';
 
-export function getCharacters(searchValue: string) {
+export function getCharacters(
+  searchValue: string,
+  updateLoading: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  updateLoading(true);
   return fetch(`${baseURL}/character/?name=${searchValue}`)
     .then((response) => {
       if (response.ok) {
@@ -13,7 +17,11 @@ export function getCharacters(searchValue: string) {
     });
 }
 
-export function getCharacter(id: number) {
+export function getCharacter(
+  id: number,
+  updateLoading: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  updateLoading(true);
   return fetch(`${baseURL}/character/${id}`)
     .then((response) => {
       return response.json();
