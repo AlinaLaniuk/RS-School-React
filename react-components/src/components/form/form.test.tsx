bbcc5b82c7b43bc8e1dfb70f321d460cf437784d
AlinaLaniuk <alinavoronina16@gmail.com>
2023-04-11 18:20:54 +0300

    feat: add some tests

--- react-components/src/components/form/form.test.tsx
+++ react-components/src/components/form/form.test.tsx
@@ -1,15 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Form from './form';
+import FormPage from '../../pages/forms/formPage';
 
 test('filling form', async () => {
-  const onNewCard = () => {
-    return undefined;
-  };
-  render(<Form onNewCard={onNewCard} />);
+  vi.stubGlobal('URL', {
+    createObjectURL: () => {},
+  });
+  render(<FormPage />);
+
+  const card = screen.queryByText(/User birthday/i);
+  expect(card).toBeNull();
   const name = screen.getByLabelText<HTMLInputElement>(/name/i);
-  await userEvent.type(name, 'test');
-  expect(name.value).toEqual('test');
+  await userEvent.type(name, 'Alina Laniuk');
+  expect(name.value).toEqual('Alina Laniuk');
 
   const birthdayDate = screen.getByLabelText<HTMLInputElement>(/birthday/i);
   await userEvent.type(birthdayDate, '2000-02-20');
@@ -28,16 +32,28 @@ test('filling form', async () => {
   expect(chocolate.checked).toEqual(true);
 
   const fileInput = screen.getByLabelText<HTMLInputElement>(/picture/i);
-  const testFile = new File(['(⌐□_□)'], 'test.png', { type: 'image/png' });
+  const testFile = new File(['test'], 'test.png', { type: 'image/png' });
   await userEvent.upload(fileInput, testFile);
+
   expect(fileInput.files).toHaveLength(1);
+
+  const submitButton = screen.getByText<HTMLInputElement>(/Submit/i);
+  expect(submitButton).toBeInTheDocument();
+  await userEvent.click(submitButton);
+  await waitFor(() => expect(screen.findByText(/Submit successfully/i)).toBeTruthy());
+  const card1 = screen.getByText(/User birthday/i);
+  expect(card1).toBeInTheDocument();
+
+  expect(name.value).toEqual('');
+  expect(birthdayDate.value).toEqual('');
+  expect(femaleInput.checked).toEqual(false);
+  expect(dessert.value).toEqual('');
+  expect(chocolate.checked).toEqual(false);
+  expect(fileInput.files).toHaveLength(0);
 });
 
 test('submit empty form', async () => {
-  const onNewCard = () => {
-    return undefined;
-  };
-  render(<Form onNewCard={onNewCard} />);
+  render(<FormPage />);
   const errorsElements = screen.queryAllByTestId('error');
   const submit = screen.getByText('Submit');
   await userEvent.click(submit);
