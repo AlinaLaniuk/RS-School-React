4fc5ed90b022c9601e8a95be07b02a91a3e7e821
AlinaLaniuk <alinavoronina16@gmail.com>
2023-04-27 15:09:20 +0300

    feat:  add renderToPipeableStream method

--- react-components/server.js
+++ react-components/server.js
@@ -15,36 +15,26 @@ async function createServer() {
     appType: 'custom',
   });
 
-  // Use vite's connect instance as middleware. If you use your own
-  // express router (express.Router()), you should use router.use
   app.use(vite.middlewares);
 
   app.use('*', async (req, res, next) => {
     const url = req.originalUrl;
     try {
       let template = fs.readFileSync(path.resolve(dirname, 'index.html'), 'utf-8');
-      // 2. Apply Vite HTML transforms. This injects the Vite HMR client,
-      //    and also applies HTML transforms from Vite plugins, e.g. global
-      //    preambles from @vitejs/plugin-react
       template = await vite.transformIndexHtml(url, template);
-      // 3. Load the server entry. ssrLoadModule automatically transforms
-      //    ESM source code to be usable in Node.js! There is no bundling
-      //    required, and provides efficient invalidation similar to HMR.
+      const html = template.split(`<!--ssr-outlet-->`);
       const { render } = await vite.ssrLoadModule(path.resolve(dirname, 'src/entry-server.tsx'));
-
-      // 4. render the app HTML. This assumes entry-server.js's exported
-      //     `render` function calls appropriate framework SSR APIs,
-      //    e.g. ReactDOMServer.renderToString()
-      const appHtml = await render(url);
-
-      // 5. Inject the app-rendered HTML into the template.
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
-
-      // 6. Send the rendered HTML back.
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+      const { pipe } = await render(url, {
+        onShellReady() {
+          res.write(html[0]);
+          pipe(res);
+        },
+        onAllReady() {
+          res.write(html[1]);
+          res.end();
+        },
+      });
     } catch (e) {
-      // If an error is caught, let Vite fix the stack trace so it maps back
-      // to your actual source code.
       vite.ssrFixStacktrace(e);
       next(e);
     }
--- react-components/src/entry-server.tsx
+++ react-components/src/entry-server.tsx
@@ -1,19 +1,19 @@
-import ReactDOMServer from 'react-dom/server';
+import { RenderToPipeableStreamOptions, renderToPipeableStream } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
 
-interface IRenderProps {
-  path: string;
-}
-
-export const render = ({ path }: IRenderProps) => {
-  return ReactDOMServer.renderToString(
+export const render = (
+  path: string | Partial<Location>,
+  options: RenderToPipeableStreamOptions | undefined
+) => {
+  return renderToPipeableStream(
     <Provider store={store}>
       <StaticRouter location={path}>
         <App />
       </StaticRouter>
-    </Provider>
+    </Provider>,
+    options
   );
 };
